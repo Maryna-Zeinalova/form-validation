d0bdfb17034bfafcda608ddbf836942df47cfaad
Maryna Zeinalova <shmapetto@gmail.com>
2023-03-02 18:21:46 +0200

    initial commit

---
+++ script/script.js
@@ -0,0 +1,23 @@
+const userNameInput = document.getElementById('user-name');
+const phoneInput = document.getElementById('phone');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const submitButton = document.getElementById('submit');
+const input = document.querySelectorAll('input');
+
+function validateStyle(condition, element){
+    if (condition){
+        element.classList.remove('invalid')
+        element.classList.add('valid');
+    }else{
+        element.classList.remove('valid');
+        element.classList.add('invalid'); 
+    }
+}
+
+
+submitButton.addEventListener('click', function(event){
+    event.preventDefault();
+})
+
+
